c081b6bfab2ea619c6ace984c215b855b16adf45
VinayaAugusthy <vinayaaugusthy@gmail.com>
2024-02-26 17:43:32 +0530

    onTap of BottomNavigationBar

--- lib/views/home/views/home_screen.dart
+++ lib/views/home/views/home_screen.dart
@@ -1,16 +1,23 @@
 import 'dart:math';
 
 import 'package:expendid/views/home/views/main_screen.dart';
+import 'package:expendid/views/stats/stats.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+int index = 0;
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(),
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.vertical(
           top: Radius.circular(30),
@@ -20,6 +27,11 @@ class HomeScreen extends StatelessWidget {
           showSelectedLabels: false,
           showUnselectedLabels: false,
           elevation: 3,
+          onTap: (value) {
+            setState(() {
+              index = value;
+            });
+          },
           items: const [
             BottomNavigationBarItem(
               icon: Icon(CupertinoIcons.home),
@@ -39,19 +51,16 @@ class HomeScreen extends StatelessWidget {
           width: 60,
           decoration: BoxDecoration(
             shape: BoxShape.circle,
-            gradient: LinearGradient(
-              colors: [
-                Theme.of(context).colorScheme.tertiary,
-                Theme.of(context).colorScheme.secondary,
-                Theme.of(context).colorScheme.primary,
-              ],
-              transform: const GradientRotation(pi/4)
-            ),
+            gradient: LinearGradient(colors: [
+              Theme.of(context).colorScheme.tertiary,
+              Theme.of(context).colorScheme.secondary,
+              Theme.of(context).colorScheme.primary,
+            ], transform: const GradientRotation(pi / 4)),
           ),
           child: const Icon(CupertinoIcons.add),
         ),
       ),
-      body: const MainScreen(),
+      body: index == 0 ? const MainScreen() : const StatScreen(),
     );
   }
 }
